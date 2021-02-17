be831c734e5e6bf88527af57a1b34be02ca377c4
Chairenerose <76175773+Chairenerose@users.noreply.github.com>
2021-02-17 12:05:06 +0100

    Create Animated Clouds

--- src/Weather.js
+++ src/Weather.js
@@ -6,30 +6,34 @@ export default function Weather() {
     return (
         <div className="WeatherText" style={{
             backgroundImage: "url(../img/Sky.jpg)",
-            backgroundRepeat: "no-repeat ",
             backgroundSize: "cover",
-            borderRadius: 7
+            borderRadius: 7,
+            backgroundRepeat: "no-repeat"
         }}>
+            <div className="cloud" style={{
+                backgroundImage: "url(../img/cloud.png)",
+                borderRadius: 8,
+            }}>
 
+                <h1>Barcelona</h1>
+                <ul>
+                    <li>Monday 20:00</li>
+                    <li>Mostly Cloudy</li>
+                </ul>
 
-            <h1>Barcelona</h1>
-            <ul>
-                <li>Monday 20:00</li>
-                <li>Mostly Cloudy</li>
-            </ul>
+                <div className="row">
+                    <div className="col-6">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Golden Sun" />
+                        <span className="temperature">20</span><span>°C|°F</span>
+                    </div>
 
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Golden Sun" />
-                    <span className="temperature">20</span><span>°C|°F</span>
-                </div>
-
-                <div className="col-6">
-                    <ul>
-                        <li>Precipitation: 10%</li>
-                        <li>Humidity: 75%</li>
-                        <li>Wind: 10 km/h</li>
-                    </ul>
+                    <div className="col-6">
+                        <ul>
+                            <li> <i class="fas fa-cloud-showers-heavy"></i> Precipitation: 10%</li>
+                            <li> <i class="fas fa-tint"></i> Humidity: 75%</li>
+                            <li> <i class="fas fa-wind"></i> Wind: 10 km/h</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
